717509635b702c3e61362af0838e3fe2165b3eb6
mtufano <74215864+mtufano@users.noreply.github.com>
2023-11-15 21:04:47 +0400

    saved info and menu to csv

--- scraper/get_menus_wip.py
+++ scraper/get_menus_wip.py
@@ -6,8 +6,6 @@ import json
 class Scraper:
     def __init__(self, url: str):
         self.url = url
-        self.restaurant_info = {}
-        self.menu_items = []
 
     def fetch_and_parse(self):
         try:
@@ -22,34 +20,81 @@ class Scraper:
             print(f"HTTP Error: {e}")
         except Exception as e:
             print(f"Error: {e}")
-
-    def extract_data(self, soup):
+    
+    def extract_menu_data(self, soup):
+        menu_items = []
         script_tags = soup.find_all('script')
         for script in script_tags:
             if 'application/ld+json' in str(script):
                 try:
                     data = json.loads(script.string)
-                    if isinstance(data, dict) and 'name' in data and 'address' in data:
-                        self.restaurant_info = data
-                    elif isinstance(data, list):
-                        for item in data:
-                            if item.get('@type') == 'MenuItem':
-                                self.menu_items.append(item)
+                    if isinstance(data, dict) and 'hasMenu' in data:
+                        menu_sections = data['hasMenu']['hasMenuSection']
+                        for section in menu_sections:
+                            for item in section['hasMenuItem']:
+                                menu_items.append({
+                                    'Name': item['name'],
+                                    'Description': item.get('description', ''),
+                                    'Price': item['offers']['price'],
+                                    'Currency': item['offers']['priceCurrency']
+                                })
                 except json.JSONDecodeError:
                     continue
+        return menu_items
+    
+    def extract_restaurant_info(self, soup):
+        script_tags = soup.find_all('script')
+        for script in script_tags:
+            if 'application/ld+json' in str(script):
+                try:
+                    data = json.loads(script.string)
+                    if isinstance(data, dict) and data.get('@type') == 'Restaurant':
+                        return data
+                except json.JSONDecodeError:
+                    continue
+        return None
+    
+    def save_to_csv(self, menu_items, filename):
+        if menu_items:
+            df = pd.DataFrame(menu_items)
+            df.to_csv(filename, index=False)
+            print(f"Data saved to {filename}")
+        else:
+            print("No data to save")
 
-    def save_to_csv(self, filename):
-        df = pd.DataFrame(self.menu_items)
-        df.to_csv(filename, index=False)
-        print(f"Data saved to {filename}")
+    def save_restaurant_to_csv(self, data, filename):
+        if data:
+            # Extracting address and geo data
+            address = data['address']
+            geo = data['geo']
+
+            restaurant_info = {
+                'Name': data['name'],
+                'Cuisine': ', '.join(data['servesCuisine']),
+                'Address': ', '.join([address.get(key, '') for key in ['streetAddress', 'addressLocality', 'addressRegion', 'postalCode', 'addressCountry']]),
+                'Latitude': geo['latitude'],
+                'Longitude': geo['longitude'],
+                'Rating': data['aggregateRating']['ratingValue'],
+                'ReviewCount': data['aggregateRating']['reviewCount']
+            }
+
+            df = pd.DataFrame([restaurant_info])
+            df.to_csv(filename, index=False)
+            print(f"Restaurant data saved to {filename}")
+        else:
+            print("No restaurant data to save")
+
 
 # Example usage
 # URL of the webpage you want to scrape
-url = 'https://www.ubereats.com/gb/store/chicken-hub-holborn/Yr2-CV8-SG-YEBh7yi44Lg?diningMode=DELIVERY'
+url = 'https://www.ubereats.com/gb/store/sticksnsushi-soho/YQeBq78eUNyu_c5ZMBo_YQ?diningMode=DELIVERY&pl=JTdCJTIyYWRkcmVzcyUyMiUzQSUyMkJhcm5ldCUyMiUyQyUyMnJlZmVyZW5jZSUyMiUzQSUyMkNoSUpVNUJkbjBjWGRrZ1JrSC1SMGNpRkVRbyUyMiUyQyUyMnJlZmVyZW5jZVR5cGUlMjIlM0ElMjJnb29nbGVfcGxhY2VzJTIyJTJDJTIybGF0aXR1ZGUlMjIlM0E1MS42NTY5MjI1JTJDJTIybG9uZ2l0dWRlJTIyJTNBLTAuMTk0OTI1MiU3RA%3D%3D'
 scraper = Scraper(url)
 soup = scraper.fetch_and_parse()
 if soup:
-    scraper.extract_data(soup)
-    scraper.save_to_csv('menu_items.csv')
-
+    menu_items = scraper.extract_menu_data(soup)
+    restaurant_data = scraper.extract_restaurant_info(soup)
+    scraper.save_restaurant_to_csv(restaurant_data, 'restaurant_info.csv')
+    scraper.save_to_csv(menu_items, 'menu.csv')
+else:
+    print("Failed to fetch or parse the webpage")
 
